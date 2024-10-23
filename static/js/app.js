d77d5c869de8795330d951b5849efc89466db160
Frederick Roegiers <frederick@rogerthat.be>
2024-10-23 11:49:13 +0200

    spannende grafiek

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,29 @@
-// alert('MQTT goes here');
+const xValues = ["11:30:00", "11:30:05", "11:30:10"]; 
+const yValues = [20, 21, 22];
+
+const chart = new Chart("myChart", {
+    type: "bar",
+    data: {
+    labels: xValues,
+    datasets: [{
+        backgroundColor: "red",
+        data: yValues
+    }]
+    },
+    options: {
+        legend: {display: false},
+        title: {
+            display: true,
+            text: "Temperatuurregistratie"
+        },
+        scales: {
+            y: {
+                min: 15,
+                max: 40
+            }
+        }
+    }
+});
 
 // MQTT client
 const mqttPath = "ws://mqtt.eclipseprojects.io/mqtt";
@@ -22,5 +47,19 @@ client.on("connect", () => {
 // MQTT message
 client.on("message", (topic, message) => {
     const data = message.toString();
-    console.log("Temperature: ", data);
+    const temp = parseFloat(data);
+    console.log("MQTT message", temp);
+
+    // update chart directly
+    chart.data.datasets[0].data.push(temp);
+    chart.data.labels.push(new Date().toLocaleTimeString());
+    chart.update();
+
+    // keep only last 5
+    const maxLen = 5;
+    if (chart.data.datasets[0].data.length > maxLen) {
+        chart.data.datasets[0].data.shift();
+        chart.data.labels.shift();
+        chart.update();
+    }
 });
