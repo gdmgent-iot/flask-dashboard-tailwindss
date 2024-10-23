54eeebf6b9915b21b77c872f4656c9e489d567ec
Frederick Roegiers <frederick@rogerthat.be>
2024-10-23 12:03:52 +0200

    updates

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,7 @@
 const xValues = []; 
 const yValues = [];
 
-const chart = new Chart("myChart", {
+const temperatureChart = new Chart("temperatureChart", {
     type: "bar",
     data: {
     labels: xValues,
@@ -49,33 +49,36 @@ client.on("message", (topic, message) => {
     const data = message.toString();
     const temp = parseFloat(data);
     console.log("MQTT message", temp);
+    console.log('Topic: ', topic);
 
-    // update dataset
-    updateDataset(temp);
+    if(topic == 'IOT/temperature') {
+        // update dataset
+        updateDataset(temperatureChart, temp);
 
-    // limit to x bars max
-    limitToXBars(10);
+        // limit to x bars max
+        limitToXBars(temperatureChart, 10);
 
-    // give cool colors to chart
-    giveCoolColorsToBars();
+        // give cool colors to chart
+        giveCoolColorsToBars(temperatureChart);
 
-    // update chart
-    chart.update();
+        // update chart
+        temperatureChart.update();
+    }
 });
 
-function updateDataset(temp) {
+function updateDataset(chart, temp) {
     chart.data.datasets[0].data.push(temp);
     chart.data.labels.push(new Date().toLocaleTimeString());
 }
 
-function limitToXBars(maxLen = 5) {
+function limitToXBars(chart, maxLen = 5) {
     if (chart.data.datasets[0].data.length > maxLen) {
         chart.data.datasets[0].data.shift();
         chart.data.labels.shift();
     }
 }
 
-function giveCoolColorsToBars() {
+function giveCoolColorsToBars(chart) {
     // temp > 27 make red
     // temp between 25 & 27 make orange
     // temp < 25 make green
